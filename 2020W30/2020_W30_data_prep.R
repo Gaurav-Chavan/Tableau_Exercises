8ddabcf197b63be0c86938fa0a3d553350f49840
Gaurav Chavan <gauravgchavan91@gmail.com>
2020-08-20 23:11:01 +0530

    Add files via upload

--- 2020W30/2020_W30_data_prep.R
+++ 2020W30/2020_W30_data_prep.R
@@ -1,51 +1,164 @@
 library(tidyverse)
+library(plotly)
 
-Viz5_August_Female_Political_Representation <- read.csv("~/Viz5_August_Female_Political_Representation.csv", na.strings="", stringsAsFactors=FALSE)
+
+Female_Political_Representation <- read.csv("Viz5_August_Female_Political_Representation.csv", na.strings="", stringsAsFactors=FALSE)
+colnames(Female_Political_Representation)[4] <-"Proportion of Seats"
+
+head(Female_Political_Representation)
 
 
 # Dealing with missing values ----
-colSums(is.na(Viz5_August_Female_Political_Representation))
+colSums(is.na(Female_Political_Representation))
 
 # Missing values within group
-missing_data <- filter(Viz5_August_Female_Political_Representation, is.na(Proportion.of.seats.held.by.women.in.national.parliaments....)) %>%
-  group_by(Country.Name,Year) %>%       
+missing_data <- filter(Female_Political_Representation, is.na(`Proportion of Seats`)) %>%
+  group_by(Year) %>%       
   summarise(count = n()) %>%
   arrange(desc(count))
 
 
+# Imputing Missing Value ----
+
+Imputed_data <- Female_Political_Representation %>% 
+                group_by(Country.Name,Country.Code) %>% # Group at Country Level
+                fill(`Proportion of Seats`, .direction = "downup") # Missing Value Imputation  
+  
+colSums(is.na(Imputed_data)) # check for completeness
+
+
 # difference vs Year Ago Compute ----
 
-# The missing values are inconsistently missing by position within the group. 
-# Use .direction = "downup" to fill missing values in both directions
-
-
-temp2 <- Viz5_August_Female_Political_Representation %>% 
+KPI1 <- Imputed_data %>% 
          group_by(Country.Name,Country.Code) %>%
-         fill(Proportion.of.seats.held.by.women.in.national.parliaments...., .direction = "downup") %>%  # Missing Value Imputation  
-         mutate(Diff_vs_Year_Ago = Proportion.of.seats.held.by.women.in.national.parliaments.... - lag(Proportion.of.seats.held.by.women.in.national.parliaments....)) %>% # Difference vs Year Ago
+         mutate(Diff_vs_Year_Ago = `Proportion of Seats` - lag(`Proportion of Seats`)) %>% # Difference vs Year Ago
          fill(Diff_vs_Year_Ago, .direction = "downup") %>%  # Missing Value Imputation  
-         mutate(Proportion.of.seats.held.by.women.in.national.parliaments.... = round(Proportion.of.seats.held.by.women.in.national.parliaments.... *100,2),
-                Diff_vs_Year_Ago = round(Diff_vs_Year_Ago*100,2)
+         mutate(`Proportion of Seats` = round(`Proportion of Seats` *100,2),
+                Diff_vs_Year_Ago = round(Diff_vs_Year_Ago*100,2) # Data Standardization
                 )  
 
 
+# Rank the differences at Yearly level ----
+Rank1 <- KPI1 %>% 
+         group_by(Year) %>% 
+         mutate(rank_Diff_vs_Year_Ago = rank( - Diff_vs_Year_Ago, ties.method = "first"))
+
+
+
+# Plot Top 10 Countries ----
+top_10_plot <- Rank1 %>% 
+  filter(Year == 2019 & rank < 11) %>% 
+  ggplot(aes(x=reorder(Country.Name, Diff_vs_Year_Ago), y= Diff_vs_Year_Ago,fill = Country.Code)) +
+  geom_bar(stat="identity")+
+  theme_minimal() +
+  theme(legend.position = "none")  +
+  xlab("Country") + 
+  coord_flip()
+
+# Convert to plotly
+ggplotly(top_10_plot)
+
+
+# Plot Bottom 10 Countries ----
+bottom_10_plot <- Rank1 %>% 
+  filter(Year == 2019 & rank >205) %>% 
+  ggplot(aes(x=reorder(Country.Name, - Diff_vs_Year_Ago), y= Diff_vs_Year_Ago,fill = Country.Code)) +
+  geom_bar(stat="identity")+
+  theme_minimal() +
+  theme(legend.position = "none")  +
+  xlab("Country") + 
+  coord_flip()
+
+# Convert to plotly
+ggplotly(bottom_10_plot)
+
+
+
+
 # Difference vs start Compute ----
-temp2 <- temp2 %>%
+KPI1 <- Rank1 %>%
   arrange(Year) %>%
   group_by(Country.Name) %>%
-  mutate(Diff_vs_Start = Proportion.of.seats.held.by.women.in.national.parliaments.... - first(Proportion.of.seats.held.by.women.in.national.parliaments....))
+  mutate(Diff_vs_Start = `Proportion of Seats` - first(`Proportion of Seats`))
 
 
-# Compute Rank basis Diff_vs_Year_Ago,Diff_vs_Start ----
-temp3 <- temp2 %>% 
-         group_by(Country.Name) %>% 
-         mutate(Rank_basis_Diff_vs_Year_Ago = order(order(Diff_vs_Year_Ago, decreasing=TRUE)),
-                Rank_basis_Diff_vs_Start = order(order(Diff_vs_Start, decreasing=TRUE))
-                )
+# Compute Rank basis at Year Yearly Basis
+Rank2 <- KPI1 %>% 
+         group_by(Year) %>% 
+         mutate(rank_Diff_vs_Start = rank( - Diff_vs_Start, ties.method = "first"))
 
 
-colnames(temp3)[1] <-  "Country Name"
-colnames(temp3)[2] <-  "Country Code"  
-colnames(temp3)[4] <- "Proportion_of_seats"
 
-write.csv(temp3,"~/Clean_data.csv",row.names = F)
+# Plot Top 10 Countries -----
+
+top_10_plot1 <- Rank2 %>% 
+  filter(Year == 2019 & rank_Diff_vs_Start < 11) %>% 
+  ggplot(aes(x=reorder(Country.Name, Diff_vs_Start), y= Diff_vs_Start,fill = Country.Code)) +
+  geom_bar(stat="identity")+
+  theme_minimal() +
+  theme(legend.position = "none")  +
+  xlab("Country") + 
+  coord_flip()
+
+# Convert to plotly
+ggplotly(top_10_plot1)
+
+
+# Plot Bottom 10 Countries ----
+bottom_10_plot1 <- Rank2 %>% 
+  filter(Year == 2019 & rank_Diff_vs_Start > 205) %>% 
+  ggplot(aes(x=reorder(Country.Name, - Diff_vs_Start), y= Diff_vs_Start,fill = Country.Code)) +
+  geom_bar(stat="identity")+
+  theme_minimal() +
+  theme(legend.position = "none")  +
+  xlab("Country") + 
+  coord_flip()
+
+# Convert to plotly
+ggplotly(bottom_10_plot1)
+
+
+# Global Level Trend ----
+Global_Insight <- KPI1 %>% 
+                  group_by(Year) %>% 
+                  summarize(
+                    Avg_Proportion_of_Seats = mean(`Proportion of Seats`),
+                    Avg_Diff_vs_Year_Ago = mean(Diff_vs_Year_Ago),
+                    Avg_Diff_vs_Start = mean(Diff_vs_Start)
+                    
+                  )
+
+
+# Area plot
+Avg_Proportion_of_Seats_plot <-  ggplot(Global_Insight, aes(x = Year, y = Avg_Proportion_of_Seats)) + 
+  geom_area(fill ="#00AFBB", alpha = 0.5, position = position_dodge(0.8))
+
+ggplotly(Avg_Proportion_of_Seats_plot)
+
+
+Avg_Diff_vs_Year_Ago_plot <-  ggplot(Global_Insight, aes(x = Year, y = Avg_Diff_vs_Year_Ago)) + 
+  geom_area(fill ="#ff7a00", alpha = 0.5, position = position_dodge(0.8))
+
+ggplotly(Avg_Diff_vs_Year_Ago_plot)
+
+Avg_Diff_vs_Start_plot <-  ggplot(Global_Insight, aes(x = Year, y = Avg_Diff_vs_Start)) + 
+  geom_area(fill ="#0dff00", alpha = 0.5, position = position_dodge(0.8))
+
+ggplotly(Avg_Diff_vs_Start_plot)
+
+# Combine 3 plots ----
+library(ggpubr)
+
+theme_set(theme_pubr())
+
+figure <- ggarrange(Avg_Proportion_of_Seats_plot, Avg_Diff_vs_Year_Ago_plot, Avg_Diff_vs_Start_plot,
+                    labels = c("Avg_Proportion_of_Seats", "Avg_Diff_vs_Year_Ago", "Avg_Diff_vs_Start"),
+                    ncol = 1, nrow = 3)
+
+figure
+
+colnames(Rank2)[1] <-  "Country Name"
+colnames(Rank2)[2] <-  "Country Code"  
+colnames(Rank2)[4] <- "Proportion_of_seats"
+
+write.csv(temp3,"Clean_data.csv",row.names = F)
